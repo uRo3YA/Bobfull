4647a32748b53b1519394e30b0da156dd376bc0e
Yoonsik-Shin <sys25800994@gmail.com>
2022-11-28 16:07:31 +0900

    refact : react 기본세팅
    - 프록시 작성
    - cors에서 막힘

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>연습</h1>
+            <div>
+                <button
+                    onClick={() => {
+                        axios.get("http://127.0.0.1:8000/articles/review/1").then((res) => {
+                            console.log(res.data);
+                        });
+                    }}
+                >
+                    버튼
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
