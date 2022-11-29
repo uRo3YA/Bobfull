ea5e3077683eb6968850c656ad72627238cd5a16
kmk4162 <kmk41623@gmail.com>
2022-11-30 02:22:39 +0900

    fix : 로그인/회원가입 방식을 jwt 방식으로 바꿈
    추후 소셜 로그인 쉽게 하기위해서 바꿨습니다

--- accounts/models.py
+++ accounts/models.py
@@ -1,62 +1,74 @@
-from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+# 헬퍼 클래스
 class UserManager(BaseUserManager):
     # 일반 user 생성
-    def create_user(self, email, nickname, name, password=None):
+    def create_user(self, email, password, nickname, name, **kwargs):
         if not email:
             raise ValueError('must have user email')
-        if not nickname:
-            raise ValueError('must have user nickname')
-        if not name:
-            raise ValueError('must have user name')
         user = self.model(
-            email = self.normalize_email(email),
+            email = email,
             nickname = nickname,
-            name = name
+            name = name,
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
     # 관리자 user 생성
-    def create_superuser(self, email, nickname, name, password=None):
-        user = self.create_user(
-            email,
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        superuser = self.create_user(
+            email = email,
             password = password,
-            nickname = nickname,
-            name = name
         )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
+        superuser.is_staff = True
+        superuser.is_superuser = True
+        superuser.is_active = True
 
-class User(AbstractBaseUser):
+        superuser.alcohol = False
+        superuser.talk = False
+        superuser.smoke = False
+        superuser.speed = 3
+        superuser.gender = False
+        superuser.manner = 36.5
+
+        superuser.set_password(password)
+        superuser.save(using=self._db)
+        return superuser
+
+# AbstractBaseUser를 상속해서 유저 커스텀   
+class User(AbstractBaseUser, PermissionsMixin):
     id = models.AutoField(primary_key=True)
-    email = models.EmailField(default='', max_length=100, null=False, blank=False, unique=True)
-    nickname = models.CharField(default='', max_length=100, null=False, blank=False, unique=True)
-    name = models.CharField(default='', max_length=100, null=False, blank=False)
+    email = models.EmailField(max_length=100, null=False, blank=False, unique=True)
+    nickname = models.CharField(default="", max_length=100, null=False, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
+    # 추가정보
+    alcohol = models.BooleanField(default=False, null=True)    
+    talk = models.BooleanField(default=False, null=True)    
+    smoke = models.BooleanField(default=False, null=True)    
+    speed = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default = 3, null=True)   
+    gender = models.BooleanField(default=False, null=True) # False가 남자    
+    manner = models.FloatField(default=36.5, null=True)  
+
     # User 모델의 필수 field
+    is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)    
-    is_admin = models.BooleanField(default=False)
-    
+    is_superuser = models.BooleanField(default=False)
+
     # 헬퍼 클래스 사용
     objects = UserManager()
 
-    # 사용자의 username field는 nickname으로 설정
-    USERNAME_FIELD = 'nickname'
-    # 필수로 작성해야하는 field
-    REQUIRED_FIELDS = ['email', 'name']
+    # 사용자의 username field는 email로 설정
+    USERNAME_FIELD = 'email'
 
-    # 추가정보
-    alcohol = models.BooleanField(default=False)    
-    talk = models.BooleanField(default=False)    
-    smoke = models.BooleanField(default=False)    
-    speed = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default = 3)   
-    gender = models.BooleanField(default=False) # False가 남자    
-    manner = models.FloatField(default=36.5)    
+    # 필수로 작성해야하는 field
+    # REQUIRED_FIELDS = ['email', 'name']
+
     def __str__(self):
         return self.nickname
 
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,14 +4,20 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['nickname', 'email', 'name', 'password']
+        # fields = ['nickname', 'email', 'name', 'password']
+        fields = '__all__'
         
     def create(self, validated_data):
         user = User.objects.create_user(
             email = validated_data['email'],
             nickname = validated_data['nickname'],
             name = validated_data['name'],
-            password = validated_data['password']
+            password = validated_data['password'],
+            alcohol = validated_data['alcohol'],
+            talk = validated_data['talk'],
+            smoke = validated_data['smoke'],
+            speed = validated_data['speed'],
+            gender = validated_data['gender'],
         )
         return user
     
