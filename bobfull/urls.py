a164c0a862b8f82defb457dc165b5392f512033a
yaonggod <jy23698@naver.com>
2022-11-30 14:01:47 +0900

    feat: restaurant viewset 생성

--- bobfull/urls.py
+++ bobfull/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
     path('user/', include('accounts.urls')),
-]
+    path('restaurant/', include('restaurant.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
