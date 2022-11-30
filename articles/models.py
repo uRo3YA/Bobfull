6dc2225da32d52a6809975aca8fc250516f27ec4
uRo3YA <ltg8693@gmail.com>
2022-11-30 14:35:25 +0900

    fix : 로그인 기능 제외하고 작동하게

--- articles/models.py
+++ articles/models.py
@@ -8,6 +8,15 @@ star_Choices = (
     ("⭐⭐⭐⭐", "⭐⭐⭐⭐"),
     ("⭐⭐⭐⭐⭐", "⭐⭐⭐⭐⭐"),
 )
+eval_Choices=(
+    ("매너가 좋았어요.","매너가 좋았어요."),
+    ("제 시간에 맞춰 왔어요.","제 시간에 맞춰 왔어요."),
+    ("여기는 뭐 추가?","여기는 뭐 추가?"),
+    ("아이디어 없음","아이디어 없음"),
+)
+
+
+
 ## 식당 후기를 작성
 class Review(models.Model):
     title = models.CharField(max_length=50)
@@ -34,7 +43,13 @@ class Matching_room(models.Model):
     content = models.TextField()
     # members = models.ManyToManyField(settings.AUTH_USER_MODEL, symmetrical=False, related_name='members')
     member = models.ManyToManyField(User, symmetrical=False, related_name='members')
-# class Member(models.Model):
-#     matching_room =  models.ForeignKey(Matching_room, null=False, blank=False, on_delete=models.CASCADE)
-#     user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
-#     # member = models.ManyToManyField(settings.AUTH_USER_MODEL, symmetrical=False, related_name='members')
+
+## 같이 간 사람의 평가
+class person_review(models.Model):
+    ## 방 번호
+    matching_room=models.ForeignKey(Matching_room,on_delete=models.CASCADE)
+    ## 작성자
+    user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    ## 평가요소
+    evaluation=models.TextField(choices=eval_Choices)
+    ## 
