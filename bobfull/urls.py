2b4587f0f29be39228d59dad41a9ea1b1d3618e9
kmk4162 <kmk41623@gmail.com>
2022-12-05 02:44:05 +0900

    fix : 전체 유저 볼 수 있게 수정, 권한/인증 일부 수정

--- bobfull/urls.py
+++ bobfull/urls.py
@@ -17,10 +17,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
+from accounts.views import UserViewSet
+
+# 유저 전체 정보 확인
+# http://127.0.0.1:8000/user/로 접속
+router = routers.DefaultRouter()
+router.register('user', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
+    path('accounts/', include('dj_rest_auth.urls')),
+    # path('accounts/', include('dj_rest_auth.registration.urls')),
+    path('accounts/', include('allauth.urls')),
     path('restaurant/', include('restaurant.urls')),
+    path('', include(router.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
