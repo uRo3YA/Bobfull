5bcab3f5f6fbaaa90f43c873821f65f932f90804
kmk4162 <kmk41623@gmail.com>
2022-12-08 22:12:12 +0900

    fix : 배포 1차 수정

--- bobfull/settings.py
+++ bobfull/settings.py
@@ -59,7 +59,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'django-environ',
+    'environ',
 
     # CORS
     'corsheaders',
