be93de271a355f696e1d03f4702f6b5b7627f59f
kmk4162 <kmk41623@gmail.com>
2022-12-12 17:03:01 +0900

    fix : 레스토랑 전체 보기 admin 권한 부여

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics, mixins, status
+from rest_framework import generics, mixins, permissions, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -19,8 +19,8 @@ class RestaurantPagination(LimitOffsetPagination):
     default_limit = 10
 
 class RestaurantViewSet(ModelViewSet):
-#    authentication_classes = [JWTAuthentication]
    queryset = Restaurant.objects.all()
+   permission_classes = [permissions.IsAdminUser]
    serializer_class = RestaurantSerializer
    pagination_class = RestaurantPagination
    filter_backends = [SearchFilter, DjangoFilterBackend]
